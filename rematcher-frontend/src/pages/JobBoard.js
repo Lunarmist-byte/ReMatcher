32d28c92383f3f7c49ab27d5c2aa4bd892cf41f4
Lunarmist <amal.s.kumar2005@gmail.com>
2025-06-08 23:48:46 +0530

    Cleaned up

--- rematcher-frontend/src/pages/JobBoard.js
+++ rematcher-frontend/src/pages/JobBoard.js
@@ -1,28 +1,28 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const JobBoard=()=>{
-    const[jobs,setJobs]=useState([]);
+const JobBoard = () => {
+  const [jobs, setJobs] = useState([]);
 
-useEffect(()=>{
+  useEffect(() => {
     axios.get('http://localhost:5000/jobs')
-    .then(res=>setJobs(res.data))
-    .catch(err=>console.error(err))
-},[]);
-}
-    return(
-        <div className='container mt-4'>
-            <h3>Job Listings</h3>
-            <ul className='list-group'>
-                {jobs.map((job,index)=>(
-                 <li className="list-group-item" key={index}>
-                  <h5>{job.title} @ {job.company}</h5>
-                  <p>{job.description}</p>
-            <     strong>Skills:</> {job.skills.join(', ')}
-              </li>
-               ))}
-            </ul>
-        </div>
+      .then(res => setJobs(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className='container mt-4'>
+      <h3>Job Listings</h3>
+      <ul className='list-group'>
+        {jobs.map((job, index) => (
+          <li className="list-group-item" key={index}>
+            <h5>{job.title} @ {job.company}</h5>
+            <p>{job.description}</p>
+            <strong>Skills:</strong> {job.skills.join(', ')}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
